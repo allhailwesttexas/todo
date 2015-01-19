c17153c9503862e0c5924b8334bcf408903008ef
Peter Stensmyr <peter.stensmyr@gmail.com>
2015-01-20 10:23:08 +1100

    don't need username anymore + work with spaces

--- todo.bat
+++ todo.bat
@@ -1,3 +1,7 @@
-set username=stensmyr
-
-copy %1 c:\Users\%username%\Desktop\
+:: The todo-list app to end all todo-list apps
+:: Where the magic happens
+:: This app puts an empty file with the name specified 
+:: on the desktop. This is the task. when you have
+:: completed the task, delete the file.
+:: Productivity: skyrocketing.
+copy /y NUL ""c:\users\%USERNAME%\Desktop\%1"" >NUL
